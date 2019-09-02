4772ff0ef61dbf484e6ef8e204f3786978bebb79
Qiu Cheng <qiucheng@sightp.com>
2019-09-02 14:02:06 +0800

    利用PolygonCollider2D解决不规则图形的精确点击功能

---
+++ Assets/3.IrregularShapeClick/script/CustomImage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomImage : Image
+{
+    private PolygonCollider2D _polygon;
+
+    public PolygonCollider2D Polygon
+    {
+        get
+        {
+            if(_polygon == null)
+            {
+                _polygon = GetComponent<PolygonCollider2D>();
+            }
+            return _polygon;
+        }
+    }
+
+    public override bool IsRaycastLocationValid(Vector2 screenPoint, Camera eventCamera)
+    {
+        Vector3 Point;
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(rectTransform, screenPoint, eventCamera, out Point);
+        return Polygon.OverlapPoint(Point);
+    }
+}
