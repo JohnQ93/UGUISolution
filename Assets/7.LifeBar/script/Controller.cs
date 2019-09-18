adfcb2a1638dfd2e27895dd00025c4789ed41c03
John_Q <qiucheng93@163.com>
2019-09-18 17:23:31 +0800

    多条血条案列实现

---
+++ Assets/7.LifeBar/script/Controller.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private LifeBar _bar;
+    void Start()
+    {
+        Canvas canvas = FindObjectOfType<Canvas>();
+
+        if(canvas == null)
+        {
+            Debug.Log("场景中没有Canvas组件");
+            return;
+        }
+
+        SpawnLifeBar(canvas);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Move(Vector3.left);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Move(Vector3.right);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Move(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            Move(Vector3.down);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            _bar.ChangeLife(-50);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            _bar.ChangeLife(50);
+        }
+    }
+
+    private void Move(Vector3 move)
+    {
+        transform.Translate(move * Time.deltaTime * 5);
+    }
+
+    private void SpawnLifeBar(Canvas canvas)
+    {
+        GameObject prefab = Resources.Load<GameObject>("LifeBar");
+        _bar = Instantiate(prefab, canvas.transform).AddComponent<LifeBar>();
+        List<LifeBarData> data = new List<LifeBarData>()
+        {
+            new LifeBarData(null, Color.red),
+            new LifeBarData(null, Color.yellow),
+            new LifeBarData(null, Color.blue)
+        };
+        _bar.init(transform, 350, data);
+    }
+}
---
+++ Assets/7.LifeBar/script/LifeBar.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeBar : MonoBehaviour
+{
+    private Transform _target;
+    private Vector3 _offset;
+    private List<LifeBarData> _data;
+    private LifeBarItem _nextBar;
+    private LifeBarItem _currentBar;
+    private float _ratio;
+    private int _currentIndex;
+    public void init(Transform target, int lifeMax, List<LifeBarData> data)
+    {
+        _currentIndex = 0;
+        _target = target;
+        _offset = GetOffset(target);
+        _data = data;
+        _nextBar = transform.Find("NextBar").gameObject.AddComponent<LifeBarItem>();
+        _currentBar = transform.Find("CurrentBar").gameObject.AddComponent<LifeBarItem>();
+        _nextBar.Init();
+        _currentBar.Init();
+
+        RectTransform rect = GetComponent<RectTransform>();
+        _ratio = rect.rect.width * data.Count / lifeMax; //每一滴血所代表的血条宽度
+
+        SetBarData(_currentIndex, data);
+    }
+
+    private Vector3 GetOffset(Transform target)
+    {
+        Renderer renderer = target.GetComponent<Renderer>();
+        if (renderer == null)
+            return Vector3.zero;
+
+        return Vector3.up * (renderer.bounds.max.y + 1f);
+    }
+
+    public void ChangeLife(float value)
+    {
+        float width = _currentBar.ChangeLife(value * _ratio);
+        if (width < 0 && ChangeIndex(1))
+        {
+            Exchange();
+            _currentBar.transform.SetAsLastSibling();
+            _nextBar.ResetToWidth();
+            SetBarData(_currentIndex, _data);
+            ChangeLife(width / _ratio);
+        }
+        else if(width > 0 && ChangeIndex(-1))
+        {
+            Exchange();
+            _currentBar.transform.SetAsLastSibling();
+            _currentBar.ResetToZero();
+            SetBarData(_currentIndex, _data);
+            ChangeLife(width / _ratio);
+        }
+    }
+
+    private void SetBarData(int index, List<LifeBarData> data)
+    {
+        if (index < 0 || index >= data.Count)
+            return;
+
+        _currentBar.SetData(data[index]);
+
+        if (index + 1 >= data.Count)
+        {
+            _nextBar.SetData(new LifeBarData(null, color: Color.white));
+        }
+        else
+        {
+            _nextBar.SetData(data[index + 1]);
+        }
+    }
+
+    /// <summary>
+    /// 改变当前下标
+    /// </summary>
+    /// <param name="symbol">1代表扣血，-1代表加血</param>
+    /// <returns></returns>
+    private bool ChangeIndex(int symbol)
+    {
+        int index = _currentIndex + symbol;
+        if(index >= 0 && index < _data.Count)
+        {
+            _currentIndex = index;
+            return true;
+        }
+
+        return false;
+    }
+
+    private void Exchange()
+    {
+        var temp = _nextBar;
+        _nextBar = _currentBar;
+        _currentBar = temp;
+    }
+
+    private void Update()
+    {
+        if (_target == null)
+            return;
+        //设置血条显示位置
+        transform.position = Camera.main.WorldToScreenPoint(_target.position + _offset);
+    }
+}
+
+public struct LifeBarData
+{
+    public Sprite lifeBarSprite;
+    public Color lifeBarColor;
+
+    public LifeBarData(Sprite sprite, Color color)
+    {
+        lifeBarSprite = sprite;
+        lifeBarColor = color;
+    }
+}
